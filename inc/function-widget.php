248a677a91fbaa21fa21029ae7c9f802b2d60626
Ernan Baldomero <ernanbaldomero@users.noreply.github.com>
2025-02-12 01:37:24 +0800

    Update function-widget.php
    fix(widgets): Refactor and optimize function-widget.php for better readability and security
    
    - Updated text domain to `gwt-wordpress` for consistency.
    - Made all hardcoded strings translatable for better localization.
    - Escaped all outputs for security and compatibility.
    - Dynamically generated similar sidebars to reduce redundancy.
    - Prefixed class names with `gwt-` to avoid conflicts.
    
    Fixes #129

--- inc/function-widget.php
+++ inc/function-widget.php
@@ -1,161 +1,60 @@
 <?php
 /**
- * Register widgetized area
- * This theme uses wp_nav_menu() in one location.
+ * Register widgetized areas and navigation menus for GWT-WordPress.
  *
- * @package GWT
- * @since Government Website Template 2.0
+ * @package GWT-WordPress
+ * @since 26.0.0
  */
- 
-function gwt_wp_widgets_init() {
-	register_nav_menus( array(
-		'aux_nav'		=> __( 'Auxiliary Menu', 'gwt_wp' ),
-		'topbar_left' 	=> __( 'Left Menu Top bar', 'gwt_wp' ),
-		'topbar_right' 	=> __( 'Right Menu Top bar', 'gwt_wp' ),
-	) );
-	register_sidebar( array(
-		'name'			=> __( 'Left Sidebar', 'gwt_wp' ),
-		'id'			=> 'left-sidebar',
-		'before_widget'	=> '<aside id="%1$s" class="widget callout secondary %2$s">',
-		'after_widget'	=> '</aside>',
-		'before_title'	=> '<h3 class="widget-title">',
-		'after_title'	=> '</h3>',
-	) );
-	register_sidebar( array(
-		'name'			=> __( 'Right Sidebar', 'gwt_wp' ),
-		'id'			=> 'right-sidebar',
-		'before_widget'	=> '<aside id="%1$s" class="widget callout secondary %2$s">',
-		'after_widget'	=> '</aside>',
-		'before_title'	=> '<h3 class="widget-title">',
-		'after_title'	=> '</h3>',
-	) );
-	register_sidebar( array(
-		'name'			=> __( 'Banner Section 1', 'gwt_wp' ),
-		'id'			=> 'banner-section-1',
-		'before_widget'	=> '<div id="%1$s" class="banner-content widget anchor %2$s">',
-		'after_widget'	=> '</div>',
-		'before_title'	=> '<h3 class="widget-title">',
-		'after_title'	=> '</h3>',
-	) );
-	register_sidebar( array(
-		'name'			=> __( 'Banner Section 2', 'gwt_wp' ),
-		'id'			=> 'banner-section-2',
-		'before_widget'	=> '<div id="%1$s" class="banner-content widget anchor %2$s">',
-		'after_widget'	=> '</div>',
-		'before_title'	=> '<h3 class="widget-title">',
-		'after_title'	=> '</h3>',
-	) );
-	register_sidebar( array(
-		'name'			=> __( 'Ear Content 1', 'gwt_wp' ),
-		'id'			=> 'ear-content-1',
-		'before_widget'	=> '<div id="%1$s" class="ear-content widget anchor %2$s">',
-		'after_widget'	=> '</div>',
-		'before_title'	=> '<h3 class="widget-title">',
-		'after_title'	=> '</h3>',
-	) );
-	register_sidebar( array(
-		'name'			=> __( 'Ear Content 2', 'gwt_wp' ),
-		'id'			=> 'ear-content-2',
-		'before_widget'	=> '<div id="%1$s" class="ear-content widget anchor %2$s">',
-		'after_widget'	=> '</div>',
-		'before_title'	=> '<h3 class="widget-title">',
-		'after_title'	=> '</h3>',
-	) );
-	register_sidebar( array(
-		'name'			=> __( 'Panel Top 1', 'gwt_wp' ),
-		'id'			=> 'panel-top-1',
-		'before_widget'	=> '<div id="%1$s" class="widget %2$s">',
-		'after_widget'	=> '</div>',
-		'before_title'	=> '<h3 class="widget-title">',
-		'after_title'	=> '</h3>',
-	) );
-	register_sidebar( array(
-		'name'			=> __( 'Panel Top 2', 'gwt_wp' ),
-		'id'			=> 'panel-top-2',
-		'before_widget'	=> '<div id="%1$s" class="widget %2$s">',
-		'after_widget'	=> '</div>',
-		'before_title'	=> '<h3 class="widget-title">',
-		'after_title'	=> '</h3>',
-	) );
-	register_sidebar( array(
-		'name'			=> __( 'Panel Top 3', 'gwt_wp' ),
-		'id'			=> 'panel-top-3',
-		'before_widget'	=> '<div id="%1$s" class="widget %2$s">',
-		'after_widget'	=> '</div>',
-		'before_title'	=> '<h3 class="widget-title">',
-		'after_title'	=> '</h3>',
-	) );
-	register_sidebar( array(
-		'name'			=> __( 'Panel Top 4', 'gwt_wp' ),
-		'id'			=> 'panel-top-4',
-		'before_widget'	=> '<div id="%1$s" class="widget %2$s">',
-		'after_widget'	=> '</div>',
-		'before_title'	=> '<h3 class="widget-title">',
-		'after_title'	=> '</h3>',
-	) );
-	register_sidebar( array(
-		'name'			=> __( 'Panel Bottom 1', 'gwt_wp' ),
-		'id'			=> 'panel-bottom-1',
-		'before_widget'	=> '<div id="%1$s" class="widget %2$s">',
-		'after_widget'	=> '</div>',
-		'before_title'	=> '<h3 class="widget-title">',
-		'after_title'	=> '</h3>',
-	) );
-	register_sidebar( array(
-		'name'			=> __( 'Panel Bottom 2', 'gwt_wp' ),
-		'id'			=> 'panel-bottom-2',
-		'before_widget'	=> '<div id="%1$s" class="widget %2$s">',
-		'after_widget'	=> '</div>',
-		'before_title'	=> '<h3 class="widget-title">',
-		'after_title'	=> '</h3>',
-	) );
-	register_sidebar( array(
-		'name'			=> __( 'Panel Bottom 3', 'gwt_wp' ),
-		'id'			=> 'panel-bottom-3',
-		'before_widget'	=> '<div id="%1$s" class="widget %2$s">',
-		'after_widget'	=> '</div>',
-		'before_title'	=> '<h3 class="widget-title">',
-		'after_title'	=> '</h3>',
-	) );
-	register_sidebar( array(
-		'name'			=> __( 'Panel Bottom 4', 'gwt_wp' ),
-		'id'			=> 'panel-bottom-4',
-		'before_widget'	=> '<div id="%1$s" class="widget %2$s">',
-		'after_widget'	=> '</div>',
-		'before_title'	=> '<h3 class="widget-title">',
-		'after_title'	=> '</h3>',
-	) );
-	register_sidebar( array(
-		'name'			=> __( 'Agency Footer 1', 'gwt_wp' ),
-		'id'			=> 'footer-1',
-		'before_widget'	=> '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'	=> '</aside>',
-		'before_title'	=> '<h3 class="widget-title">',
-		'after_title'	=> '</h3>',
-	) );
-	register_sidebar( array(
-		'name'			=> __( 'Agency Footer 2', 'gwt_wp' ),
-		'id'			=> 'footer-2',
-		'before_widget'	=> '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'	=> '</aside>',
-		'before_title'	=> '<h3 class="widget-title">',
-		'after_title'	=> '</h3>',
-	) );
-	register_sidebar( array(
-		'name'			=> __( 'Agency Footer 3', 'gwt_wp' ),
-		'id'			=> 'footer-3',
-		'before_widget'	=> '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'	=> '</aside>',
-		'before_title'	=> '<h3 class="widget-title">',
-		'after_title'	=> '</h3>',
-	) );
-	register_sidebar( array(
-		'name'			=> __( 'Agency Footer 4', 'gwt_wp' ),
-		'id'			=> 'footer-4',
-		'before_widget'	=> '<div id="%1$s" class="widget %2$s">',
-		'after_widget'	=> '</div>',
-		'before_title'	=> '<h3 class="widget-title">',
-		'after_title'	=> '</h3>',
-	) );
+
+/**
+ * Register widgetized areas and navigation menus.
+ */
+function gwt_widgets_init() {
+    // Register navigation menus.
+    register_nav_menus([
+        'aux_nav'      => esc_html__('Auxiliary Menu', 'gwt-wordpress'),
+        'topbar_left'  => esc_html__('Left Menu Top Bar', 'gwt-wordpress'),
+        'topbar_right' => esc_html__('Right Menu Top Bar', 'gwt-wordpress'),
+    ]);
+
+    // Define sidebar configurations.
+    $sidebars = [
+        'left-sidebar'   => ['name' => __('Left Sidebar', 'gwt-wordpress'), 'class' => 'gwt-widget callout secondary'],
+        'right-sidebar'  => ['name' => __('Right Sidebar', 'gwt-wordpress'), 'class' => 'gwt-widget callout secondary'],
+        'banner-section-1' => ['name' => __('Banner Section 1', 'gwt-wordpress'), 'class' => 'gwt-widget banner-content anchor'],
+        'banner-section-2' => ['name' => __('Banner Section 2', 'gwt-wordpress'), 'class' => 'gwt-widget banner-content anchor'],
+        'ear-content-1' => ['name' => __('Ear Content 1', 'gwt-wordpress'), 'class' => 'gwt-widget ear-content anchor'],
+        'ear-content-2' => ['name' => __('Ear Content 2', 'gwt-wordpress'), 'class' => 'gwt-widget ear-content anchor'],
+    ];
+
+    // Add panel top and bottom sidebars dynamically.
+    foreach (['panel-top', 'panel-bottom'] as $section) {
+        for ($i = 1; $i <= 4; $i++) {
+            $sidebars["$section-$i"] = [
+                'name' => sprintf(__('Panel %s %d', 'gwt-wordpress'), ucfirst(str_replace('-', ' ', $section)), $i),
+                'class' => 'gwt-widget',
+            ];
+        }
+    }
+
+    // Add agency footer sidebars dynamically.
+    foreach (range(1, 4) as $i) {
+        $sidebars["footer-$i"] = [
+            'name' => sprintf(__('Agency Footer %d', 'gwt-wordpress'), $i),
+            'class' => 'gwt-widget',
+        ];
+    }
+
+    // Register all sidebars.
+    foreach ($sidebars as $id => $config) {
+        register_sidebar([
+            'name'          => $config['name'],
+            'id'            => $id,
+            'before_widget' => '<div id="%1$s" class="' . esc_attr($config['class']) . ' %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="gwt-widget-title">',
+            'after_title'   => '</h3>',
+        ]);
+    }
 }
-add_action( 'widgets_init', 'gwt_wp_widgets_init' );
+add_action('widgets_init', 'gwt_widgets_init');
